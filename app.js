f58a8258a73a361423d772768d39b82c2d3a1fc7
ealvaro <alvaroescobaronline@gmail.com>
2016-06-01 10:27:23 -0400

    Added more than one gem thru ng-repeat

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 (function(){
-  var app = angular.module('store', [ ]);
-  app.controller('StoreController', function(){
-    this.product = gem;
-  });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'super jewel'
-  };
+var app = angular.module('store', [ ]);
+
+app.controller('StoreController', function(){
+this.products = gems;
+});
+
+var gems = [{
+  name: 'Dodecahedron',
+  price: 2.95,
+  description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems.',
+  canPurchase: false,
+  soldOut: false
+  },
+  {
+  name: "Pentagonal Gem",
+  price: 5.95,
+  description: "Origin of the Pentagonal gem is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+  canPurchase: false,
+  soldOut: false
+}];
 
 })();
