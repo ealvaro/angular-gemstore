79324ca8bdd0e07262c4ac383e9e57ce90f614bb
ealvaro <alvaroescobaronline@gmail.com>
2016-06-02 01:25:07 -0400

    Added gem tabs for description,specs and reviews

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var gems = [{
   name: 'Dodecahedron',
   price: 2.95,
   description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems.',
-  canPurchase: false,
+  canPurchase: true,
   soldOut: false,
   images: [{
     full: 'dodecahedron-01-full.gif',
@@ -25,7 +25,7 @@ var gems = [{
   name: "Pentagonal Gem",
   price: 5.95,
   description: "Origin of the Pentagonal gem is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-  canPurchase: false,
+  canPurchase: true,
   soldOut: false,
   images: [{
     full: 'pentagonal-01-full.gif',
@@ -40,7 +40,7 @@ var gems = [{
   name: "Azurite Gem",
   price: 577.95,
   description: "Origin of this precious stone is South Africa and has a very high value. It has a very high shine and 8 sides.",
-  canPurchase: false,
+  canPurchase: true,
   soldOut: false,
   images: [{
     full: 'azurite-01-full.gif',
